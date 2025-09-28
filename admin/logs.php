5c82347dfa94a32de574481e41dd0493c27418a5
HAJAR AL-HASHEMI <hajaralh@mass4.om>
2025-09-29 03:54:57 +0400

    securty logs file

---
+++ admin/logs.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../includes/functions.php';
+require_role('admin');
+check_session_timeout();
+require_once __DIR__ . '/../includes/header.php';
+?>
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
